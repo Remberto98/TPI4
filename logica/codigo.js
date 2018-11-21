a2fa83994e7342ae02bbedbc14570f4107dab182
Fernando Alvarado <fer25alvarado@gmail.com>
2018-11-20 20:20:32 -0600

    Responsive, bootstrap 4

--- logica/codigo.js
+++ logica/codigo.js
@@ -3,7 +3,9 @@
 /*
   Videos con los que voy a trabajar:
 
-    Zelda & Chill ---> ID: GdzrrWA8e7A
+    Zelda & Chill -----------------------> ID: GdzrrWA8e7A
+    Official Orchestrated Zelda Music ---> ID: asHAvqwZ1Ig
+    Luna - Don Omar ---------------------> ID: _hdrVZ-7o-0
     
 */
 
@@ -14,16 +16,18 @@ function cambiarVideo(enlace, autoreproducir) {
   } else {
     src += enlace + "?autoplay=0";
   }
-  document.getElementById('iframe-video-principal').src = "";
-  document.getElementById('iframe-video-principal').src = src;
+  document.getElementById("iframe-video-principal").src = "";
+  document.getElementById("iframe-video-principal").src = src;
 }
 
 function obtenerID_YT(enlaceYT) {
   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-  var match = url.match(regExp);
-  return (match&&match[7].length==11)? match[7] : false;
+  var match = enlaceYT.match(regExp);
+  return match && match[7].length == 11 ? match[7] : false;
 }
 
-cambiarVideo("asHAvqwZ1Ig", true);
-alert("Ronald es la vieja puta! xdxdxd");
+function resizeIframe(obj) {
+  obj.style.height = obj.contentWindow.document.body.scrollHeight + "px";
+}
+
 
